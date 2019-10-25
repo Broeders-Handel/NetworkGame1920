81cccac86046d8b07c8736d0b7eb36f856b4ad76
ll-26147 <ll-26147@broeders.be>
2019-10-25 12:03:56 +0200

    form gemaakt in client

---
+++ src/ChatClient/ChatClient/TCPControllerClient.vb
@@ -0,0 +1,21 @@
+﻿Imports System.ComponentModel
+Imports System.Net.Sockets
+Imports System.IO
+Public Class TCPControllerClient
+    Dim TCPClient As TcpClient
+    Dim TCPClientStream As NetworkStream
+    Dim RX As StreamReader
+    Dim TX As StreamWriter
+    Public Sub Connect()
+        Try
+            TCPClient = New TcpClient("127.0.0.1", 64553)
+            TCPClientStream = TCPClient.GetStream()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Enum ClientStatus
+        Connected
+        Disconnected
+    End Enum
+End Class
