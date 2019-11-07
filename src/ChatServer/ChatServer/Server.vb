82252751ab9115ae435b970a606326325cb23c1d
Jansegers Eli <ll-12115@broeders.be>
2019-11-07 20:20:28 +0100

    Threads bijgemaakt

--- src/ChatServer/ChatServer/Server.vb
+++ src/ChatServer/ChatServer/Server.vb
@@ -1,12 +1,28 @@
 ﻿Imports System.Net.Sockets
 Imports System.Net
 Imports System.IO
+Imports System.Threading.Thread
 
 Public Class Server
+    Dim ThreadConnectClient As System.Threading.Thread
     Dim TCPServer As Socket
     Dim TCPListener As TcpListener
     Dim serverStatus As Boolean = False
     Dim UsersController As New UsersController
+
+
+    Private Sub ConnectClient()
+        Try
+            TCPListener = New TcpListener(IPAddress.Any, 64553)
+            TCPListener.Start()
+            TCPServer = TCPListener.AcceptSocket()
+            TCPServer.Blocking = False
+            Timer1.Enabled = True
+            serverStatus = True
+        Catch ex As Exception
+            serverStatus = False
+        End Try
+    End Sub
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         Try
             Dim rcvbytes(TCPServer.ReceiveBufferSize) As Byte
@@ -41,18 +57,8 @@ Public Class Server
         SendToClient(MessageTextBox.Text)
     End Sub
     Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
-        If serverStatus = False Then
-            Try
-                TCPListener = New TcpListener(IPAddress.Any, 64553)
-                TCPListener.Start()
-                TCPServer = TCPListener.AcceptSocket()
-                TCPServer.Blocking = False
-                Timer1.Enabled = True
-                serverStatus = True
-            Catch ex As Exception
-                serverStatus = False
-            End Try
-        End If
+        ThreadConnectClient = New System.Threading.Thread(AddressOf ConnectClient)
+        ThreadConnectClient.Start()
         If serverStatus = True Then
             ChatRichTextBox.Text &= "<< NEW USER CONNECTED >>" & Environment.NewLine
         End If
--- src/ChatServer/ChatServer/UsersController.vb
+++ src/ChatServer/ChatServer/UsersController.vb
@@ -1,5 +1,16 @@
-﻿Public Class UsersController
+﻿Imports System.Net.Sockets
+Class UsersController
     Private _usersList As New List(Of Users)
+    Private _ClientsList As New List(Of TcpClient)
+
+    Public Property ClientsList As List(Of TcpClient)
+        Get
+            Return _ClientsList
+        End Get
+        Set(value As List(Of TcpClient))
+            _ClientsList = value
+        End Set
+    End Property
 
     Public Property UsersList As List(Of Users)
         Get
@@ -11,7 +22,10 @@
     End Property
     Public Sub addUser(username As String)
         Dim Users As New Users
-        Users.username = username
+        Users.Username = username
         _usersList.Add(Users)
+    End Sub
+    Public Sub AddClient()
+
     End Sub
 End Class
